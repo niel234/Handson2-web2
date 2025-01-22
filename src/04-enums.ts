edc9c2d0cd20b9cd9afa4e907ab59c6cbd7efd04
niel234 <2382022@unai.edu>
2025-01-22 08:01:16 -0800

    Update 04-enums.ts

---
+++ src/04-enums.ts
@@ -0,0 +1,25 @@
+/** 
+ *TypeScript Enums
+ *Enum digunakan untuk mendefinisikan sekumpulan nilai yang bernama dan bersifat tetap,
+ * sehingga memudahkan representasi dan manipulasi data dengan nilai konstan.
+
+// Contoh enum Direction
+*/
+
+// Numberic enumx`
+enum Direction {
+    Up = 1,
+    Down,
+    Left,
+    Right,
+  }
+  
+// String enum
+enum color {
+    Red = "RED",
+    Green = "GREEN",
+    Blue = "BLUE",
+}
+
+console.log{color.Red};
+  
